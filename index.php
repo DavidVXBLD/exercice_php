62a64de742ad1d807b07abee2c241a1ebef52a7e
DavidV <david.vieuxbled@outlook.fr>
2021-12-01 16:09:28 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<h2>Les types de donnéees</h2>
+<p>Exercice 1 de Thomgo</p>
+
+<?php 
+    echo "26";
+?>
+
+<!-- Le type de donnée affiché est: string -->
+
+<p>Exercice 2 de Thomgo</p>
+
+<?php 
+    echo 'Hello World';
+?>
+
+<p>Exercice 3 de Thomgo</p>
+
+<?php 
+    echo TRUE;
+?>
